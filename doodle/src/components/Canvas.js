75773a9f0b1e4fa6e7cd8a371abcb211c74436c3
gabeboileau <gabeboileau@gmail.com>
2020-03-07 15:33:15 -0500

    Adds batching for the lines and gets it ready to be sent.

--- doodle/src/components/Canvas.js
+++ doodle/src/components/Canvas.js
@@ -9,23 +9,52 @@ export function Canvas(props) {
 
   let canvasRefObject = useRef();
 
+  // Different stroke styles to be used for user and guest
+  let userStrokeStyle = "#EE92C2";
+  let guestStrokeStyle = "#F0C987";
+  let line = [];
+
+  // v4 creates a unique id for each user. We used this since there's no auth to tell users apart
+  let prevPos = { x: 0, y: 0 };
+
   function mouseMove(e) {
     if (!drawing) return;
+
     let rect = canvasRefObject.getBoundingClientRect();
     let context = canvasRefObject.getContext("2d");
 
-    context.lineWidth = 2;
-    context.lineCap = "round";
+    const offsetX = e.clientX - rect.left;
+    const offsetY = e.clientY - rect.top;
 
-    const xPosition = e.clientX - rect.left;
-    const yPosition = e.clientY - rect.top;
+    const pos = { offsetX, offsetY };
 
-    context.lineTo(xPosition, yPosition);
+    // Set the start and stop position of the paint event.
+    const positionData = {
+      start: { ...prevPos },
+      stop: { ...pos }
+    };
+    // Add the position to the line array
+    line = line.concat(positionData);
+    paint(pos, userStrokeStyle);
+  }
 
-    context.stroke();
+  function paint(currPos, strokeStyle) {
+    let context = canvasRefObject.getContext("2d");
+
+    const { offsetX, offsetY } = currPos;
+    const { offsetX: x, offsetY: y } = prevPos;
+
     context.beginPath();
+    context.strokeStyle = strokeStyle;
 
-    context.moveTo(xPosition, yPosition);
+    // Move the the prevPosition of the mouse
+    context.moveTo(x, y);
+    // Draw a line to the current position of the mouse
+    context.lineTo(offsetX, offsetY);
+    // Visualize the line using the strokeStyle
+    context.stroke();
+
+    prevPos = { offsetX, offsetY };
   }
 
   function beginDraw(e) {
@@ -36,6 +65,28 @@ export function Canvas(props) {
     setDrawing(false);
     let context = canvasRefObject.getContext("2d");
     context.beginPath();
+
+    // after we're done drawing - we're going to send the data
+    sendPaintData();
+  }
+
+  function sendPaintData() {
+    // send the paint data to the API
+    const body = {
+      line: line
+    };
+    console.log("Body to be sent");
+    console.log(body);
+    // We use the native fetch API to make requests to the server
+    // const req = await fetch('http://localhost:4000/paint', {
+    //   method: 'post',
+    //   body: JSON.stringify(body),
+    //   headers: {
+    //     'content-type': 'application/json',
+    //   },
+    // });
+    // const res = await req.json();
+    line = [];
   }
 
   useEffect(() => {}, []);
